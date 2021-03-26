da5d60c566494c75358d41d4c9f2d10f732def81
spatiality-dc <dan@dinosaurmedia.org>
2021-03-27 00:33:48 +1100

    rough view of idea

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // Initial array of movies
-var movies = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
+let movies = ["The Matrix", "The Notebook", "Mr Robot", "The Lion King"];
 
 // displayMovieInfo function re-renders the HTML to display the appropriate content
 function displayMovieInfo() {
 
-  var movie = $(this).attr("data-name");
-  var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+  function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+  let container = document.querySelector('#movies-view');
+  removeAllChildNodes(container);
+
+  let movie = $(this).attr("data-name");
+  let queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
 
   // Creating an AJAX call for the specific movie button being clicked
   $.ajax({
@@ -14,40 +22,40 @@ function displayMovieInfo() {
   }).then(function(response) {
 
     // Creating a div to hold the movie
-    var movieDiv = $("<div class='movie'>");
+    let movieDiv = $("<div class='movie'>");
 
     // Storing the rating data
-    var rating = response.Rated;
+    let rating = response.Rated;
 
     // Creating an element to have the rating displayed
-    var pOne = $("<p>").text("Rating: " + rating);
+    let pOne = $("<p>").text("Rating: " + rating);
 
     // Displaying the rating
     movieDiv.append(pOne);
 
     // Storing the release year
-    var released = response.Released;
+    let released = response.Released;
 
     // Creating an element to hold the release year
-    var pTwo = $("<p>").text("Released: " + released);
+    let pTwo = $("<p>").text("Released: " + released);
 
     // Displaying the release year
     movieDiv.append(pTwo);
 
     // Storing the plot
-    var plot = response.Plot;
+    let plot = response.Plot;
 
     // Creating an element to hold the plot
-    var pThree = $("<p>").text("Plot: " + plot);
+    let pThree = $("<p>").text("Plot: " + plot);
 
     // Appending the plot
     movieDiv.append(pThree);
 
     // Retrieving the URL for the image
-    var imgURL = response.Poster;
+    let imgURL = response.Poster;
 
     // Creating an element to hold the image
-    var image = $("<img>").attr("src", imgURL);
+    let image = $("<img>").attr("src", imgURL);
 
     // Appending the image
     movieDiv.append(image);
@@ -66,13 +74,12 @@ function renderButtons() {
   $("#buttons-view").empty();
 
   // Looping through the array of movies
-  for (var i = 0; i < movies.length; i++) {
+  for (let i = 0; i < movies.length; i++) {
 
-    // Then dynamicaly generating buttons for each movie in the array
-    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-    var a = $("<button>");
-    // Adding a class of movie-btn to our button
-    a.addClass("movie-btn");
+    // Dynamically generating buttons for each movie in the array
+    let a = $("<button>");
+    // Adding a class of btn-outline-secondary
+    a.addClass("btn-outline-secondary");
     // Adding a data-attribute
     a.attr("data-name", movies[i]);
     // Providing the initial button text
@@ -80,13 +87,15 @@ function renderButtons() {
     // Adding the button to the buttons-view div
     $("#buttons-view").append(a);
   }
+    //Set session storage
+
 }
 
 // This function handles events where a movie button is clicked
 $("#add-movie").on("click", function(event) {
   event.preventDefault();
   // This line grabs the input from the textbox
-  var movie = $("#movie-input").val().trim();
+  let movie = $("#movie-input").val().trim();
 
   // Adding movie from the textbox to our array
   movies.push(movie);
@@ -95,8 +104,12 @@ $("#add-movie").on("click", function(event) {
   renderButtons();
 });
 
-// Adding a click event listener to all elements with a class of "movie-btn"
-$(document).on("click", ".movie-btn", displayMovieInfo);
+// Adding a click event listener to all elements with a class of "btn-outline-secondary"
+$(document).on("click", ".btn-outline-secondary", displayMovieInfo);
+
 
 // Calling the renderButtons function to display the initial buttons
 renderButtons();
+
+
+
